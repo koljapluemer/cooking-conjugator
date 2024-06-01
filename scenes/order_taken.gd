a2b00d86888cce572c65d890cba8012fb5c2b57c
blackbrokkoli <kastomat@gmail.com>
2024-06-01 19:10:49 +0200

    detect word finished

--- scenes/order_taken.gd
+++ scenes/order_taken.gd
@@ -2,11 +2,15 @@ extends HBoxContainer
 
 var parent_exercise: Exercise
 
-@onready var en_ui: Label = $VBoxContainer/En
+@onready var prompt: Label = $VBoxContainer/HBoxContainer/Prompt
+@onready var verb: Label = $VBoxContainer/HBoxContainer/Verb
+@onready var result: Label = $VBoxContainer/ResultBox/Result
+
 
 @onready var chars_ui: HBoxContainer = $VBoxContainer/Chars
 const CHAR = preload("res://scenes/char.tscn")
 var chars = []
+var resulting_word = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,7 +23,8 @@ func _process(delta: float) -> void:
 
 func set_exercise(exercise: Exercise) -> void:
 	parent_exercise = exercise
-	en_ui.text = exercise.en + ", " + exercise.verb.eg_script
+	verb.text = exercise.verb.eg_script
+	prompt.text = exercise.en
 	
 	if exercise.eg_script_only_intro.length() > 0:
 		var intro = exercise.eg_script_only_intro
@@ -38,15 +43,22 @@ func set_exercise(exercise: Exercise) -> void:
 			char.set_values(exercise.exercise_initial[i][0], SLOT_TYPE.REPLACE, exercise.eg_script_chars[i])
 		else: 
 			char.set_values(exercise.exercise_template[index][0], SLOT_TYPE.IMMUTABLE)
-
+		
+		char.connect("drag_finished", _on_letter_drag_finished)
 		chars.append(char)
 		
-#func _can_drop_data(at_position, data):
-	#print("received offer for parent")
-	#return true
-		#
-		#
-		#
-		#
+	calculate_resulting_word()
+
+func _on_letter_drag_finished():
+	calculate_resulting_word()	
+	if resulting_word == parent_exercise.eg_script_only_verb:
+		print("you win")
+	
+func calculate_resulting_word():
+	resulting_word = ""
+	for char in chars:
+		resulting_word += char.label.text
+	result.text = parent_exercise.eg_script_only_intro + resulting_word
+	
 		
 		
