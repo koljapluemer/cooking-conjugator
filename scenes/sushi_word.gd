8b609fd086d0ff867abb17bc1a148b8c59f3ecc8
blackbrokkoli <kastomat@gmail.com>
2024-06-01 16:19:07 +0200

    make sushi words dragable

---
+++ scenes/sushi_word.gd
@@ -0,0 +1,26 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _get_drag_data(at_position):
+	var preview = duplicate()
+	set_drag_preview(preview)
+	visible = false	
+	
+	return text
+	
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_DRAG_END and not get_viewport().gui_is_drag_successful():
+		print("resetting...")
+		reset()
+	
+func reset():
+	visible = true
