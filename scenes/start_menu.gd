1615992413d0143f8f9034b95c8a06b46b6b5c49
blackbrokkoli <kastomat@gmail.com>
2024-06-14 19:02:53 +0300

    change scene routing

--- scenes/start_menu.gd
+++ scenes/start_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 const UI = preload("res://scenes/ui.tscn")
-
+const TUTORIAL = preload("res://scenes/tutorial_ui.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,3 +13,7 @@ func _process(delta: float) -> void:
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_packed(UI)
+
+
+func _on_tutorial_button_pressed() -> void:
+	get_tree().change_scene_to_packed(TUTORIAL)
